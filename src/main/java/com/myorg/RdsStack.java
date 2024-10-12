f7ea2540be7436ce234da0ca8de24961625c32ee
wandersonsantos01 <~wanderson.santos01@gmail.com>
2024-10-12 11:03:51 -0300

    Adding RDS stack

---
+++ src/main/java/com/myorg/RdsStack.java
@@ -0,0 +1,67 @@
+package com.myorg;
+
+import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.*;
+import software.amazon.awscdk.services.ec2.InstanceType;
+import software.amazon.awscdk.services.rds.*;
+import software.constructs.Construct;
+
+import java.util.Collections;
+
+public class RdsStack extends Stack {
+    public RdsStack(final Construct scope, final String id, Vpc vpc) {
+        this(scope, id, null, vpc);
+    }
+
+    public RdsStack(final Construct scope, final String id, final StackProps props, Vpc vpc) {
+        super(scope, id, props);
+
+        CfnParameter databaseUsername = CfnParameter.Builder.create(this, "databaseUsername")
+                .type("String")
+                .description("RDS instance username")
+                .build();
+
+        CfnParameter databasePassword = CfnParameter.Builder.create(this, "databasePassword")
+                .type("String")
+                .description("RDS instance password")
+                .build();
+
+        ISecurityGroup iSecurityGroup = SecurityGroup.fromSecurityGroupId(this, id, vpc.getVpcDefaultSecurityGroup());
+        iSecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(3306)); // Allow connections to the database only inside VPC
+
+        DatabaseInstance databaseInstance = DatabaseInstance.Builder
+                .create(this, "Rds01")
+                .instanceIdentifier("aws-project01-db")
+                .engine(DatabaseInstanceEngine.mysql(MySqlInstanceEngineProps.builder()
+                        .version(MysqlEngineVersion.VER_8_0)
+                        .build()))
+                .vpc(vpc)
+                .credentials(Credentials.fromUsername(databaseUsername.getValueAsString(),
+                        CredentialsFromUsernameOptions.builder()
+                                .password(SecretValue.unsafePlainText(databasePassword.getValueAsString()))
+                                .build()))
+                .instanceType(InstanceType.of(InstanceClass.T3, InstanceSize.MICRO))
+                .multiAz(false)
+                .allocatedStorage(10)
+                .securityGroups(Collections.singletonList(iSecurityGroup))
+                .vpcSubnets(SubnetSelection.builder()
+                        .subnets(vpc.getPrivateSubnets())
+                        .build())
+                .build();
+
+        CfnOutput.Builder.create(this, "rds-endpoint")
+                .exportName("rds-endpoint")
+                .value(databaseInstance.getDbInstanceEndpointAddress())
+                .build();
+
+        CfnOutput.Builder.create(this, "rds-username")
+                .exportName("rds-username")
+                .value(databaseUsername.getValueAsString())
+                .build();
+
+        CfnOutput.Builder.create(this, "rds-password")
+                .exportName("rds-password")
+                .value(databasePassword.getValueAsString())
+                .build();
+    }
+}
