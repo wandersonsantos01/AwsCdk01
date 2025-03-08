ad5ddfcfff407467a47bd0f16faad75a7062da43
wandersonsantos01 <~wanderson.santos01@gmail.com>
2025-03-08 16:12:22 -0300

    Adding SNS env variables and permissions

--- src/main/java/com/myorg/Service01Stack.java
+++ src/main/java/com/myorg/Service01Stack.java
@@ -6,24 +6,28 @@ import software.amazon.awscdk.services.ecs.*;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
 import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
+import software.amazon.awscdk.services.events.targets.SnsTopic;
 import software.amazon.awscdk.services.logs.LogGroup;
+import software.amazon.awscdk.services.logs.RetentionDays;
 import software.constructs.Construct;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Service01Stack extends Stack {
-    public Service01Stack(final Construct scope, final String id, Cluster cluster) {
-        this(scope, id, null, cluster);
+    public Service01Stack(final Construct scope, final String id, Cluster cluster, SnsTopic productEventsTopic) {
+        this(scope, id, null, cluster, productEventsTopic);
     }
 
-    public Service01Stack(final Construct scope, final String id, final StackProps props, Cluster cluster) {
+    public Service01Stack(final Construct scope, final String id, final StackProps props, Cluster cluster, SnsTopic productEventsTopic) {
         super(scope, id, props);
 
         Map<String, String> envVariables = new HashMap<>();
         envVariables.put("SPRING_DATASOURCE_URL", "jdbc:mariadb://" + Fn.importValue("rds-endpoint") + ":3306/aws_project01?createDatabaseIfNotExist=true");
         envVariables.put("SPRING_DATASOURCE_USERNAME", Fn.importValue("rds-username"));
-        envVariables.put("SPRING_DATASOURCE_PASSWORD", Fn.importValue("rds-endpoint"));
+        envVariables.put("SPRING_DATASOURCE_PASSWORD", Fn.importValue("rds-password"));
+        envVariables.put("AWS_REGION", "us-east-1");
+        envVariables.put("AWS_SNS_TOPIC_PRODUCT_EVENTS_ARN", productEventsTopic.getTopic().getTopicArn());
 
         ApplicationLoadBalancedFargateService service01 = ApplicationLoadBalancedFargateService.Builder
                 .create(this, "ALB01")
@@ -42,6 +46,7 @@ public class Service01Stack extends Stack {
                                 .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
                                         .logGroup(LogGroup.Builder.create(this,"Service01LogGroup")
                                                 .logGroupName("Service01")
+                                                .retention(RetentionDays.ONE_WEEK)
                                                 .removalPolicy(RemovalPolicy.DESTROY)
                                                 .build())
                                         .streamPrefix("Service01")
@@ -69,5 +74,6 @@ public class Service01Stack extends Stack {
                 .scaleOutCooldown(Duration.seconds(60))
                 .build());
 
+        productEventsTopic.getTopic().grantPublish(service01.getTaskDefinition().getTaskRole());
     }
 }
